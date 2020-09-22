debb2575bb02e43790ad4405ec3694b96b4e84a9
kobdani <kobdani@yahoo.com>
2020-09-22 16:33:10 +0200

    add demo jaxb

---
+++ java/jaxb/demo/demo/AddressType.java
@@ -0,0 +1,91 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2020.09.22 um 04:22:14 PM CEST 
+//
+
+
+package demo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für AddressType complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="AddressType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Number" type="{http://www.w3.org/2001/XMLSchema}unsignedInt"/>
+ *         &lt;element name="Street" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "AddressType", propOrder = {
+    "number",
+    "street"
+})
+public class AddressType {
+
+    @XmlElement(name = "Number")
+    @XmlSchemaType(name = "unsignedInt")
+    protected long number;
+    @XmlElement(name = "Street", required = true)
+    protected String street;
+
+    /**
+     * Ruft den Wert der number-Eigenschaft ab.
+     * 
+     */
+    public long getNumber() {
+        return number;
+    }
+
+    /**
+     * Legt den Wert der number-Eigenschaft fest.
+     * 
+     */
+    public void setNumber(long value) {
+        this.number = value;
+    }
+
+    /**
+     * Ruft den Wert der street-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * Legt den Wert der street-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStreet(String value) {
+        this.street = value;
+    }
+
+}
